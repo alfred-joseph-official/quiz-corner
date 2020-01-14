860c27556489ce06aed8a3338edccabd4880450a
Mukhtar <macbookpro@MacBook-Pro.local>
2020-01-14 20:27:02 +0530

    FrontendNewPassV1.0

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var routes = express.Router()
 var mongoDB = require("mongodb")
+var crypto = require("crypto");
+var nodemailer = require("nodemailer");
 var url = "mongodb://localhost:27017"
 var dbNAME = "quiz-corner-attainu"
 var DB = ''
+
 mongoDB.MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -14,6 +17,17 @@ mongoDB.MongoClient.connect(url, {
         DB = server.db(dbNAME)
     }
 })
+
+var transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: "quiz.corner.attainu@gmail.com",
+        pass: 'jnef9820'
+    }
+});
+
 routes.get('/', function(req, res) {
     //Todo Render only homepage
     // if (req.session.user) {
@@ -31,7 +45,7 @@ routes.get('/home', function(req, res) {
 routes.post("/signupuser", function(req, res) {
     var data = {
         name: "",
-        username: req.body.usn,
+        usn: req.body.usn,
         email: req.body.email,
         pwd: req.body.pwd,
         dp: "",
@@ -50,12 +64,12 @@ routes.post("/signupuser", function(req, res) {
 
 routes.post('/loginuser', function(req, res) {
 
-    DB.collection('Users').findOne({ username: req.body.usn }, function(err, result) {
+    DB.collection('Users').findOne({ usn: req.body.usn }, function(err, result) {
         if (err) {
             res.redirect('/')
         } else {
-            if (req.body.usn == result.username && req.body.pwd == result.password) {
-                req.session.user = result.username
+            if (req.body.usn == result.usn && req.body.pwd == result.pwd) {
+                req.session.user = result.usn
                 check = false
                 res.redirect('/profile')
             }
@@ -63,6 +77,89 @@ routes.post('/loginuser', function(req, res) {
     })
 })
 
+routes.get("/forgot", function(req, res) {
+    res.render("forgot");
+});
+
+routes.post("/forgot", function(req, res) {
+    console.log(req.body.field);
+    console.log(req.body);
+
+
+    DB.collection('Users').findOne({ $or: [{ email: req.body.field }, { usn: req.body.field }] }, function(err, userObj) {
+        if (err || !userObj) {
+            // res.redirect('/');
+            res.status(400).end();
+        } else {
+            crypto.randomBytes(16, function(err, buffer) {
+                var token = buffer.toString('hex');
+                var expire = Date.now() + 1000 * 60 * 15;
+                var mailOptions = {
+                    from: '"Quiz Corner " <no-reply@quiz-corner.com',
+                    to: userObj.email,
+                    subject: "Reset Password",
+                    text: 'Hello ' + userObj.usn + '! You have requested to reset your password. Click on the below link to reset your password.' + "\nhttp://localhost:4500/reset/token/" + token
+                }
+
+                transporter.sendMail(mailOptions, function(mailErr, info) {
+                    if (mailErr) res.status(400).end();
+                    else {
+                        var newStr = {};
+                        newStr["expire_time"] = expire;
+                        newStr["token"] = token;
+                        var newValues = { $set: newStr };
+                        DB.collection('ResetLinks').findOneAndUpdate({ $or: [{ email: req.body.field }, { usn: req.body.field }] }, newValues, { returnOriginal: false }, function(err, resetObj) {
+                            if (err || !resetObj.value) {
+                                resetLink = {
+                                    usn: userObj.usn,
+                                    email: userObj.email,
+                                    expire_time: expire,
+                                    token: token
+                                }
+                                DB.collection('ResetLinks').insertOne(resetLink, function(err, result) {
+                                    if (err) {
+                                        res.status(400).end();
+                                    } else {
+                                        res.status(200).end();
+                                        // res.send('Message sent: ' + info.response);
+                                    }
+                                });
+                            } else {
+                                //todo check for err
+                                res.status(200).end();
+                                // res.send('Message sent: ' + info.response);
+                            }
+                        });
+                    }
+                })
+            });
+        }
+    });
+});
+
+routes.get("/reset/token/:t", function(req, res) {
+    var tkn = req.params.t
+    DB.collection('ResetLinks').findOne({ token: tkn }, function(err, resetObj) {
+        if (err || !resetObj) {
+            res.send("Link Not Found!"); // give 404
+        } else if (resetObj.expire_time < Date.now()) {
+            //delete resetObj from db
+            res.send("Link Expired!");
+        } else {
+            res.render('new_pass', { usn: resetObj.usn });
+        }
+    });
+});
+
+routes.post("/pwd", function(req, res) {
+    DB.collection('ResetLinks').remove({ usn: req.body.usn }, function(err, deletedRes) {
+        if (err || deletedRes.result.n < 1) { res.send("Link Expired!"); } else {
+            DB.collection('Users').findOneAndUpdate({ usn: req.body.usn }, { $set: { pwd: req.body.pwd } }, { returnOriginal: false }, function(err, result) {
+                res.redirect("/");
+            });
+        }
+    });
+});
 
 routes.get('/game', function(req, res) {
     res.render('gamepage', {
